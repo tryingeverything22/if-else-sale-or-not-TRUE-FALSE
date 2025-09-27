9c6f2d80c4c9c84358563e1cd84bb05393bfb604
tryingeverything22 <kallikciahmett83@gmail.com>
2025-09-27 18:08:40 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+#response = input("Would you like food?(Y/N):")
+
+#if response == "Y":
+#    print("Have some food!")
+#else:
+#    print("No food for you!")
+
+
+
+#name = input ("Enter your name:")
+
+#if name =="":
+#    print("You did not type in your name!")
+#else:
+#    print(f"Hello {name}")
+
+
+# for sale or not?? for_sale = True/False
+
+#for_sale = True
+#if for_sale:
+#    print("This item is for sale!")
+#else:
+#    print("This item is NOT for sale")
+
+
+# Online Offline Same logic
+
+
+#online = True
+#if online:
+#    print("You are online")
+#else:
+#    print("You are offline")
+
+
+# Note ("im new for pyhton")
